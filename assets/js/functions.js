3ecff29da0b95e9c54ba121c6507e25ec2e98b10
fabijanski <mm.fabijanski@gmail.com>
2017-03-29 22:40:52 +0200

    style message clouds

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,29 +1,29 @@
 // SETTING THE SCROLLBAR IN CONVERSATION WINDOW
-var out = document.getElementById("out");
+var conversation = document.getElementById("conversation");
 var c = 0;
-// first set scrollbar on the bottom of the page
-out.scrollTop = out.scrollHeight;
+// first set scrollbar on the bottom of the conversation window
+conversation.scrollTop = conversation.scrollHeight;
 
 var add = setInterval(function() {
     // allow 1px inaccuracy by adding 1
-    var isScrolledToBottom = out.scrollHeight - out.clientHeight <= out.scrollTop + 1;
-    // console.log(out.scrollHeight - out.clientHeight,  out.scrollTop + 1);
+    var isScrolledToBottom = conversation.scrollHeight - conversation.clientHeight <= conversation.scrollTop + 1;
+    // console.log(conversation.scrollHeight - conversation.clientHeight,  conversation.scrollTop + 1);
 
-    // add new element every second to test if everything is ok
+    // // add new element every second to test if everything is ok
     // var newElement = document.createElement("div");
     // newElement.innerHTML = c++;
-    // out.appendChild(newElement);
+    // conversation.appendChild(newElement);
 
     // scroll to bottom if isScrolledToBottom
     if(isScrolledToBottom)
-      out.scrollTop = out.scrollHeight - out.clientHeight;
+      conversation.scrollTop = conversation.scrollHeight - conversation.clientHeight;
 }, 1000);
 
-// var element = document.getElementById("out");
+// var element = document.getElementById("conversation");
 // element.scrollTop = element.scrollHeight;
 //
 // function updateScroll(){
-//     var element = document.getElementById("out");
+//     var element = document.getElementById("conversation");
 //     element.scrollTop = element.scrollHeight;
 // }
 // //once a second
