1bc508e0a7d20af44bb6a4798163dd60d7415116
fabijanski <mm.fabijanski@gmail.com>
2017-04-04 04:22:04 +0200

    add readme file

---
+++ README.md
@@ -0,0 +1,11 @@
+# Chat Module
+
+### Description
+
+The thing was to build the interface from [Dinesh's project](https://dribbble.com/shots/3146448-Chat-Module).
+
+Used: HTML, CSS, SCSS, jQuery.
+
+### Demo
+
+[Click me!](https://fabijanski.github.io/chatModule)
--- assets/js/functions.js
+++ assets/js/functions.js
@@ -60,7 +60,6 @@ function findUser() {
   // loop through all list items, and hide those who don't match the search query
   for (i = 0; i < li.length; i++) {
     a = li[i].querySelectorAll('.top-info span')[0];
-    // for exact fit type below (... === 0)
     if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
         li[i].style.display = "";
     } else {
